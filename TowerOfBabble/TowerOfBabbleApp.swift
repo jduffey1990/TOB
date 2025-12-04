f4f1ed82026ce1bf5b2ca6fcfa866d8cdf13f2b4
Jordan Duffey <jduffey1990@gmail.com>
2025-12-04 15:40:27 -0700

    Initial Commit

---
+++ TowerOfBabble/TowerOfBabbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TowerOfBabbleApp.swift
+//  TowerOfBabble
+//
+//  Created by Jordan Duffey on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct TowerOfBabbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
